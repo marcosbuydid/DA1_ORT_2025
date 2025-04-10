4f4cf37ca4ff81ff1dd254bdf97196109c04471d
marcos <marcosbuydid@gmail.com>
2025-04-10 15:17:50 -0300

    add blazor base project net8.0

---
+++ blazor_base_project_net8.0/DA12025/Domain/Movie.cs
@@ -0,0 +1,15 @@
+﻿namespace Domain;
+
+public class Movie
+{
+    public string Title { get; set; }
+    public string Director { get; set; }
+    public DateTime ReleaseDate { get; set; }
+ 
+    public Movie(string title, string director, DateTime releaseYear)
+    {
+        Title = title;
+        Director = director;
+        ReleaseDate = releaseYear;
+    }
+}
---
+++ blazor_base_project_net8.0/DA12025/Services/Interfaces/IMovieService.cs
@@ -0,0 +1,13 @@
+﻿using Domain;
+
+namespace Services.Interfaces
+{
+    public interface IMovieService
+    {
+        List<Movie> GetMovies();
+        void AddMovie(Movie movie);
+        void DeleteMovie(string title);
+        void UpdateMovie(Movie movie);
+        Movie GetMovie(string title);
+    }
+}
