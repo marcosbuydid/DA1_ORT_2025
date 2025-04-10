ba239c071c009cf712ddc5366486acabec51afa4
marcos <marcosbuydid@gmail.com>
2025-04-10 17:07:48 -0300

    add unit tests project net8.0

---
+++ unit_tests_mstest_net8.0/DA12025/DataAccess/InMemoryDatabase.cs
@@ -0,0 +1,13 @@
+﻿using Domain;
+
+namespace DataAccess;
+
+public class InMemoryDatabase
+{
+    public List<Movie> Movies { get; set; }
+
+    public InMemoryDatabase()
+    {
+        Movies = new List<Movie>();
+    }
+}
