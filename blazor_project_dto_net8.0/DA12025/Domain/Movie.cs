852351826e946995769ee569f12cc21de4b0db8f
marcos <marcosbuydid@gmail.com>
2025-04-12 12:51:44 -0300

    add project dto net8.0

---
+++ blazor_project_dto_net8.0/DA12025/Domain/Movie.cs
@@ -0,0 +1,68 @@
+﻿namespace Domain;
+
+public class Movie
+{
+    private string _title;
+    private string _director;
+    private DateTime _releaseDate;
+    private int _budget;
+
+    public string Title
+    {
+        get => _title;
+
+        set
+        {
+            if (string.IsNullOrEmpty(value))
+            {
+                throw new ArgumentException("Title cannot be null or empty");
+            }
+
+            _title = value;
+        }
+    }
+
+    public string Director
+    {
+        get => _director;
+
+        set
+        {
+            if (string.IsNullOrEmpty(value))
+            {
+                throw new ArgumentException("Director cannot be null or empty");
+            }
+
+            _director = value;
+        }
+    }
+
+    public DateTime ReleaseDate
+    {
+        get => _releaseDate;
+        set { _releaseDate = value; }
+    }
+
+    public int Budget
+    {
+        get => _budget;
+
+        set
+        {
+            if (value < 0)
+            {
+                throw new ArgumentException("Budget must be a positive number");
+            }
+
+            _budget = value;
+        }
+    }
+
+    public Movie(string title, string director, DateTime releaseYear, int budget)
+    {
+        Title = title;
+        Director = director;
+        ReleaseDate = releaseYear;
+        Budget = budget;
+    }
+}
---
+++ blazor_project_dto_net8.0/DA12025/Services/Interfaces/IMovieService.cs
@@ -0,0 +1,13 @@
+﻿using Services.Models;
+
+namespace Services.Interfaces
+{
+    public interface IMovieService
+    {
+        List<MovieDTO> GetMovies();
+        void AddMovie(MovieDTO movie);
+        void DeleteMovie(string title);
+        void UpdateMovie(MovieDTO movie);
+        MovieDTO GetMovie(string title);
+    }
+}
