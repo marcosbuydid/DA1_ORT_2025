640f0b68d4d689b1ee52e74e9cb4340b94fbd7ed
marcos <marcosbuydid@gmail.com>
2025-04-14 15:02:25 -0300

    add blazor full project ef

---
+++ blazor_full_project_ef_net8.0/DA12025/Domain.Tests/MovieTests.cs
@@ -0,0 +1,115 @@
+namespace Domain.Tests;
+
+[TestClass]
+public class MovieTests
+{
+    [TestMethod]
+    public void CreateNewMovie_WhenIdIsNotNull_ThenIdShouldBeAssigned() 
+    {
+        //arrange
+        Movie movie;
+        //act
+        movie = new Movie(1, "Gladiator 2", "Ridley Scott", new DateTime(2024, 11, 22), 250000000);
+        //assert
+        Assert.AreEqual(1, movie.Id);
+    }
+
+    [TestMethod]
+    [ExpectedException(typeof(ArgumentException))]
+    public void CreateNewMovie_WhenTitleIsNull_ThenThrowException()
+    {
+        //arrange
+        Movie movie;
+        //act
+        movie = new Movie(1, null, "Ridley Scott", new DateTime(2024, 11, 22), 250000000);
+        //assert
+    }
+
+    [TestMethod]
+    [ExpectedException(typeof(ArgumentException))]
+    public void CreateNewMovie_WhenTitleIsEmpty_ThenThrowException()
+    {
+        //arrange
+        Movie movie;
+        //act
+        movie = new Movie(1, "", "Ridley Scott", new DateTime(2024, 11, 22), 250000000);
+        //assert
+    }
+
+    [TestMethod]
+    public void CreateNewMovie_WhenTitleIsNotNullOrEmpty_ThenTitleShouldBeAssigned()
+    {
+        //arrange
+        Movie movie;
+        //act
+        movie = new Movie(1, "Gladiator 2", "Ridley Scott", new DateTime(2024, 11, 22), 250000000);
+        //assert
+        Assert.AreEqual("Gladiator 2", movie.Title);
+    }
+
+    [TestMethod]
+    [ExpectedException(typeof(ArgumentException))]
+    public void CreateNewMovie_WhenDirectorIsNull_ThenThrowException()
+    {
+        //arrange
+        Movie movie;
+        //act
+        movie = new Movie(1, "Gladiator 2", null, new DateTime(2024, 11, 22), 250000000);
+        //assert
+    }
+
+    [TestMethod]
+    [ExpectedException(typeof(ArgumentException))]
+    public void CreateNewMovie_WhenDirectorIsEmpty_ThenThrowException()
+    {
+        //arrange
+        Movie movie;
+        //act
+        movie = new Movie(1, "Gladiator 2", "", new DateTime(2024, 11, 22), 250000000);
+        //assert
+    }
+
+    [TestMethod]
+    public void CreateNewMovie_WhenDirectorIsNotNullOrEmpty_ThenDirectorShouldBeAssigned()
+    {
+        //arrange
+        Movie movie;
+        //act
+        movie = new Movie(1, "Gladiator 2", "Ridley Scott", new DateTime(2024, 11, 22), 250000000);
+        //assert
+        Assert.AreEqual("Ridley Scott", movie.Director);
+    }
+
+    [TestMethod]
+    public void CreateNewMovie_WhenReleaseDateIsEarlierThanToday_ThenReleaseDateShouldBeAssigned()
+    {
+        //arrange
+        Movie movie;
+        //act
+        movie = new Movie(1, "Gladiator 2", "Ridley Scott", new DateTime(2024, 11, 22), 250000000);
+        //assert
+        Assert.AreEqual(new DateTime(2024, 11, 22), movie.ReleaseDate);
+    }
+
+    [TestMethod]
+    [ExpectedException(typeof(ArgumentException))]
+    public void CreateNewMovie_WhenBudgetIsNegative_ThenThrowException()
+    {
+        //arrange
+        Movie movie;
+        //act
+        movie = new Movie(1, "Gladiator 2", "Ridley Scott", new DateTime(2024, 11, 22), -250000000);
+        //assert
+    }
+
+    [TestMethod]
+    public void CreateNewMovie_WhenBudgetIsPositive_ThenBudgetShouldBeAssigned()
+    {
+        //arrange
+        Movie movie;
+        //act
+        movie = new Movie(1, "Gladiator 2", "Ridley Scott", new DateTime(2024, 11, 22), 250000000);
+        //assert
+        Assert.AreEqual(250000000, movie.Budget);
+    }
+}
---
+++ blazor_full_project_ef_net8.0/DA12025/Services/Interfaces/IUserService.cs
@@ -0,0 +1,12 @@
+﻿using Services.Models;
+
+namespace Services.Interfaces;
+
+public interface IUserService
+{
+    List<UserDTO> GetUsers();
+    UserDTO GetUser(string email);
+    void AddUser(UserDTO user);
+    void DeleteUser(string email);
+    void UpdateUser(UserDTO user);
+}
