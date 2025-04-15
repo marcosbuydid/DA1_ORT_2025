7472fe9896a4a3aaba18517230b1788ceb41277b
marcos <marcosbuydid@gmail.com>
2025-04-15 20:58:59 -0300

    add the three inheritance mappings on entity framework

---
+++ example_inheritance_ef_net8.0/DA12025/DataAccess/Migrations/20250415231905_Initial.cs
@@ -0,0 +1,47 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace DataAccess.Migrations
+{
+    /// <inheritdoc />
+    public partial class Initial : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "Persons",
+                columns: table => new
+                {
+                    Id = table.Column<int>(type: "int", nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    Name = table.Column<string>(type: "nvarchar(max)", nullable: false),
+                    PersonType = table.Column<string>(type: "nvarchar(13)", maxLength: 13, nullable: false),
+                    Salary = table.Column<int>(type: "int", nullable: true),
+                    Career = table.Column<string>(type: "nvarchar(max)", nullable: true)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Persons", x => x.Id);
+                });
+
+            migrationBuilder.InsertData(
+                table: "Persons",
+                columns: new[] { "Id", "Career", "Name", "PersonType" },
+                values: new object[] { 1, "Computer Engineer", "Student Name", "Student" });
+
+            migrationBuilder.InsertData(
+                table: "Persons",
+                columns: new[] { "Id", "Name", "PersonType", "Salary" },
+                values: new object[] { 2, "Professor Name", "Professor", 1500 });
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "Persons");
+        }
+    }
+}
