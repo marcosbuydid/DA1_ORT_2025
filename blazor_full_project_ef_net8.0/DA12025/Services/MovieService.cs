afae63bfdfbee59f5fba5da742abea5b1b59e62f
marcos <marcosbuydid@gmail.com>
2025-05-22 20:27:56 -0300

    add refactors

--- blazor_full_project_ef_net8.0/DA12025/Services/MovieService.cs
+++ blazor_full_project_ef_net8.0/DA12025/Services/MovieService.cs
@@ -34,17 +34,16 @@ namespace Services
 
         public void DeleteMovie(string title)
         {
-            Movie movieToDelete = GetMovieObject(title);
+            Movie? movieToDelete = _movieRepository.Get(m => m.Title == title);
             _movieRepository.Delete(movieToDelete);
         }
 
         public void UpdateMovie(MovieDTO movieToUpdate)
         {
-            Movie movie = GetMovieObject(movieToUpdate.Title);
+            Movie? movie = _movieRepository.Get(m => m.Title == movieToUpdate.Title);
             movie.Title = movieToUpdate.Title;
             movie.Director = movieToUpdate.Director;
             movie.ReleaseDate = movieToUpdate.ReleaseDate;
-            movie.Budget = movieToUpdate.Budget;
             _movieRepository.Update(movie);
         }
 
@@ -57,17 +56,6 @@ namespace Services
             }
 
             return FromEntity(movie);
-        }
-
-        private Movie GetMovieObject(string title)
-        {
-            Movie? movie = _movieRepository.Get(movie => movie.Title == title);
-            if (movie == null)
-            {
-                throw new ArgumentException("Cannot find movie with this title");
-            }
-
-            return movie;
         }
 
         private void ValidateUniqueTitle(String title)
