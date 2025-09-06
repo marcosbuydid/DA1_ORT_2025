e2c4dcbcedb95460d380fbccae6707bd43d6cabe
marcos <marcosbuydid@gmail.com>
2025-09-06 14:25:49 -0300

    user tests green cycle

--- example_TDD_c#/DA1/Domain.Tests/UserTests.cs
+++ example_TDD_c#/DA1/Domain.Tests/UserTests.cs
@@ -4,50 +4,85 @@ namespace Domain.Tests;
 public class UserTests
 {
     [TestMethod]
+    [ExpectedException(typeof(ArgumentException))]
     public void NewUser_WhenNameIsEmpty_ThenThrowException()
     {
-        throw new NotImplementedException();
+        //arrange
+        User user;
+        //act
+        user = new User("", "test@email.com", "P@ssw0rd");
     }
 
     [TestMethod]
     public void NewUser_WhenNameIsValid_ThenNameShouldBeAssigned()
     {
-        throw new NotImplementedException();
+        //arrange
+        User user;
+        //act
+        user = new User("Username", "username@email.com", "P@ssw0rd");
+        //assert
+        Assert.AreEqual("Username", user.Name);
     }
 
     [TestMethod]
+    [ExpectedException(typeof(ArgumentException))]
     public void NewUser_WhenEmailIsEmpty_ThenThrowException()
     {
-        throw new NotImplementedException();
+        //arrange
+        User user;
+        //act
+        user = new User("Username", "", "P@ssw0rd");
     }
 
     [TestMethod]
+    [ExpectedException(typeof(ArgumentException))]
     public void NewUser_WhenEmailIsNotValid_ThenThrowException()
     {
-        throw new NotImplementedException();
+        //arrange
+        User user;
+        //act
+        user = new User("Username", "@domain.com", "P@ssw0rd");
     }
 
     [TestMethod]
     public void NewUser_WhenEmailIsValid_ThenEmailShouldBeAssigned()
     {
-        throw new NotImplementedException();
+        //arrange
+        User user;
+        //act
+        user = new User("Username", "username@domain.com", "P@ssw0rd");
+        //assert
+        Assert.AreEqual("username@domain.com", user.Email);
     }
 
     [TestMethod]
+    [ExpectedException(typeof(ArgumentException))]
     public void NewUser_WhenPasswordIsEmpty_ThenThrowException()
     {
-        throw new NotImplementedException();
+        //arrange
+        User user;
+        //act
+        user = new User("Username", "email@domain.com", "");
     }
 
     [TestMethod]
+    [ExpectedException(typeof(ArgumentException))]
     public void NewUser_WhenPasswordIsLessThanEightCharsLong_ThenThrowException()
     {
-        throw new NotImplementedException();
+        //arrange
+        User user;
+        //act
+        user = new User("Username", "email@domain.com", "passwd");
     }
 
     [TestMethod]
     public void NewUser_WhenPasswordIsValid_ThenPasswordShouldBeAssigned()
     {
-        throw new NotImplementedException();
+        //arrange
+        User user;
+        //act
+        user = new User("Username", "username@email.com", "P@ssw0rd");
+        //assert
+        Assert.AreEqual("P@ssw0rd", user.Password);
     }
 }
