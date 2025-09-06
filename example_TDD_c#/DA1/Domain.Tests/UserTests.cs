46217e33e290afa8bcc198dec517d659c5b9b14e
marcos <marcosbuydid@gmail.com>
2025-09-06 13:33:51 -0300

    user tests red cycle

---
+++ example_TDD_c#/DA1/Domain.Tests/UserTests.cs
@@ -0,0 +1,53 @@
+namespace Domain.Tests;
+
+[TestClass]
+public class UserTests
+{
+    [TestMethod]
+    public void NewUser_WhenNameIsEmpty_ThenThrowException()
+    {
+        throw new NotImplementedException();
+    }
+
+    [TestMethod]
+    public void NewUser_WhenNameIsValid_ThenNameShouldBeAssigned()
+    {
+        throw new NotImplementedException();
+    }
+
+    [TestMethod]
+    public void NewUser_WhenEmailIsEmpty_ThenThrowException()
+    {
+        throw new NotImplementedException();
+    }
+
+    [TestMethod]
+    public void NewUser_WhenEmailIsNotValid_ThenThrowException()
+    {
+        throw new NotImplementedException();
+    }
+
+    [TestMethod]
+    public void NewUser_WhenEmailIsValid_ThenEmailShouldBeAssigned()
+    {
+        throw new NotImplementedException();
+    }
+
+    [TestMethod]
+    public void NewUser_WhenPasswordIsEmpty_ThenThrowException()
+    {
+        throw new NotImplementedException();
+    }
+
+    [TestMethod]
+    public void NewUser_WhenPasswordIsLessThanEightCharsLong_ThenThrowException()
+    {
+        throw new NotImplementedException();
+    }
+
+    [TestMethod]
+    public void NewUser_WhenPasswordIsValid_ThenPasswordShouldBeAssigned()
+    {
+        throw new NotImplementedException();
+    }
+}
