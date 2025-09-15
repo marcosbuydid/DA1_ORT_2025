04a242d54369c0658227f5a9d0b432f9d65a01b2
marcos <marcosbuydid@gmail.com>
2025-09-14 23:10:42 -0300

    add refactors

--- blazor_base_project_net8.0/DA12025/Services/MovieService.cs
+++ blazor_base_project_net8.0/DA12025/Services/MovieService.cs
@@ -16,32 +16,37 @@ namespace Services
         public void AddMovie(Movie movie)
         {
             ValidateUniqueTitle(movie.Title);
-
-            _inMemoryDatabase.Movies.Add(movie);
+            _inMemoryDatabase.AddMovie(movie);
         }
 
         public void DeleteMovie(string title)
         {
             Movie movieToDelete = GetMovie(title);
-            _inMemoryDatabase.Movies.Remove(movieToDelete);
+            _inMemoryDatabase.DeleteMovie(movieToDelete);
         }
 
         public List<Movie> GetMovies()
         {
-            return _inMemoryDatabase.Movies;
+            return _inMemoryDatabase.GetMovies();
         }
 
         public void UpdateMovie(Movie movieToUpdate)
         {
-            Movie? movie = _inMemoryDatabase.Movies.Find(m => m.Title == movieToUpdate.Title);
-            var movieToUpdateIndex = _inMemoryDatabase.Movies.IndexOf(movie);
+            Movie? movie = _inMemoryDatabase.GetMovie(movieToUpdate.Title);
+            if (movie == null)
+            {
+                throw new ArgumentException("Cannot find the specified movie");
+            }
 
-            _inMemoryDatabase.Movies[movieToUpdateIndex] = movieToUpdate;
+            movie.Title = movieToUpdate.Title;
+            movie.Director = movieToUpdate.Director;
+            movie.ReleaseDate = movieToUpdate.ReleaseDate;
+            _inMemoryDatabase.UpdateMovie(movie);
         }
 
         public Movie GetMovie(string title)
         {
-            Movie? movie = _inMemoryDatabase.Movies.FirstOrDefault(movie => movie.Title == title);
+            Movie? movie = _inMemoryDatabase.GetMovie(title);
             if (movie == null)
             {
                 throw new ArgumentException("Cannot find movie with this title");
@@ -50,9 +55,9 @@ namespace Services
             return movie;
         }
 
-        private void ValidateUniqueTitle(String title)
+        private void ValidateUniqueTitle(string title)
         {
-            foreach (var movie in _inMemoryDatabase.Movies)
+            foreach (var movie in _inMemoryDatabase.GetMovies())
             {
                 if (movie.Title == title)
                 {
