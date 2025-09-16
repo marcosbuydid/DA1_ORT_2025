58f771959263bfba185717a987aff971c047ee42
marcos <marcosbuydid@gmail.com>
2025-09-15 21:39:18 -0300

    add some refactors

--- blazor_project_tests_only_net8.0/DA12025/DataAccess/InMemoryDatabase.cs
+++ blazor_project_tests_only_net8.0/DA12025/DataAccess/InMemoryDatabase.cs
@@ -4,10 +4,38 @@ namespace DataAccess;
 
 public class InMemoryDatabase
 {
-    public List<Movie> Movies { get; set; }
+    private List<Movie> Movies { get; }
 
     public InMemoryDatabase()
     {
         Movies = new List<Movie>();
+    }
+
+    public List<Movie> GetMovies()
+    {
+        return Movies;
+    }
+
+    public Movie? GetMovie(string title)
+    {
+        Movie? movie = Movies.FirstOrDefault(movie => movie.Title == title);
+        return movie;
+    }
+
+    public void AddMovie(Movie movie)
+    {
+        Movies.Add(movie);
+    }
+
+    public void DeleteMovie(Movie movie)
+    {
+        Movies.Remove(movie);
+    }
+
+    public void UpdateMovie(Movie movieToUpdate)
+    {
+        Movie? movie = Movies.Find(m => m.Title == movieToUpdate.Title);
+        var movieToUpdateIndex = Movies.IndexOf(movie);
+        Movies[movieToUpdateIndex] = movieToUpdate;
     }
 }
--- blazor_project_tests_only_net8.0/DA12025/Services/MovieService.cs
+++ blazor_project_tests_only_net8.0/DA12025/Services/MovieService.cs
@@ -4,7 +4,7 @@ using Services.Interfaces;
 
 namespace Services;
 
-public class MovieService: IMovieService
+public class MovieService : IMovieService
 {
     private readonly InMemoryDatabase _inMemoryDatabase;
 
@@ -16,42 +16,48 @@ public class MovieService: IMovieService
     public void AddMovie(Movie movie)
     {
         ValidateUniqueTitle(movie.Title);
-
-        _inMemoryDatabase.Movies.Add(movie);
+        _inMemoryDatabase.AddMovie(movie);
     }
 
     public void DeleteMovie(string title)
     {
         Movie movieToDelete = GetMovie(title);
-        _inMemoryDatabase.Movies.Remove(movieToDelete);
+        _inMemoryDatabase.DeleteMovie(movieToDelete);
     }
 
     public List<Movie> GetMovies()
     {
-        return _inMemoryDatabase.Movies;
+        return _inMemoryDatabase.GetMovies();
     }
 
     public void UpdateMovie(Movie movieToUpdate)
     {
-        Movie? movie = _inMemoryDatabase.Movies.Find(m => m.Title == movieToUpdate.Title);
-        var movieToUpdateIndex = _inMemoryDatabase.Movies.IndexOf(movie);
+        Movie? movie = _inMemoryDatabase.GetMovie(movieToUpdate.Title);
+        if (movie == null)
+        {
+            throw new ArgumentException("Cannot find the specified movie");
+        }
 
-        _inMemoryDatabase.Movies[movieToUpdateIndex] = movieToUpdate;
+        movie.Title = movieToUpdate.Title;
+        movie.Director = movieToUpdate.Director;
+        movie.ReleaseDate = movieToUpdate.ReleaseDate;
+        _inMemoryDatabase.UpdateMovie(movie);
     }
 
     public Movie GetMovie(string title)
     {
-        Movie? movie = _inMemoryDatabase.Movies.FirstOrDefault(movie => movie.Title == title);
+        Movie? movie = _inMemoryDatabase.GetMovie(title);
         if (movie == null)
         {
             throw new ArgumentException("Cannot find movie with this title");
         }
+
         return movie;
     }
 
-    private void ValidateUniqueTitle(String title)
+    private void ValidateUniqueTitle(string title)
     {
-        foreach (var movie in _inMemoryDatabase.Movies)
+        foreach (var movie in _inMemoryDatabase.GetMovies())
         {
             if (movie.Title == title)
             {
