c4f162cc4eb58cb6234224df75e6a05730313034
marcos <marcosbuydid@gmail.com>
2025-09-16 18:53:00 -0300

    add some refactors

--- blazor_project_dto_net8.0/DA12025/DataAccess/InMemoryDatabase.cs
+++ blazor_project_dto_net8.0/DA12025/DataAccess/InMemoryDatabase.cs
@@ -4,12 +4,40 @@ namespace DataAccess
 {
     public class InMemoryDatabase
     {
-        public List<Movie> Movies { get; set; }
+        private List<Movie> Movies { get; }
 
         public InMemoryDatabase()
         {
             Movies = new List<Movie>();
             LoadDefaultMovies();
+        }
+
+        public List<Movie> GetMovies()
+        {
+            return Movies;
+        }
+
+        public Movie? GetMovie(string title)
+        {
+            Movie? movie = Movies.FirstOrDefault(movie => movie.Title == title);
+            return movie;
+        }
+
+        public void AddMovie(Movie movie)
+        {
+            Movies.Add(movie);
+        }
+
+        public void DeleteMovie(Movie movie)
+        {
+            Movies.Remove(movie);
+        }
+
+        public void UpdateMovie(Movie movieToUpdate)
+        {
+            Movie? movie = Movies.Find(m => m.Title == movieToUpdate.Title);
+            var movieToUpdateIndex = Movies.IndexOf(movie);
+            Movies[movieToUpdateIndex] = movieToUpdate;
         }
 
         private void LoadDefaultMovies()
