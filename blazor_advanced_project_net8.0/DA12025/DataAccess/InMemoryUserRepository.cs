9f3e2753c2f56a262ce514b7c4a222633802fb88
marcos <marcosbuydid@gmail.com>
2026-03-16 19:44:20 -0300

    add refactors

---
+++ blazor_advanced_project_net8.0/DA12025/DataAccess/InMemoryUserRepository.cs
@@ -0,0 +1,49 @@
+﻿using DataAccess.Interfaces;
+using Domain;
+
+namespace DataAccess;
+
+public class InMemoryUserRepository: IUserRepository
+{
+        private List<User> Users { get; }
+
+        public InMemoryUserRepository()
+        {
+            Users = new List<User>();
+            LoadDefaultAdministratorUser();
+        }
+
+        private void LoadDefaultAdministratorUser()
+        {
+            Users.Add(new User(null, "Marcos", "Buydid", "marcosb@email.com",
+                "fTjXowEsKZzCX8K9YYq3FXG9eztFfl2fuOa6buT59tUHUutO8C7z49UnTUIaA0Zr", "Administrator"));
+        }
+
+        public List<User> GetUsers()
+        {
+            return Users;
+        }
+
+        public User? GetUser(Func<User, bool> filter)
+        {
+            return Users.Where(filter).FirstOrDefault();
+        }
+
+        public void AddUser(User user)
+        {
+            Users.Add(user);
+        }
+
+        public void UpdateUser(User userToUpdate)
+        {
+            User? user = Users.Find(u => u.Email == userToUpdate.Email);
+            var userToUpdateIndex = Users.IndexOf(user);
+            userToUpdate.Password = user.Password;
+            Users[userToUpdateIndex] = userToUpdate;
+        }
+
+        public void DeleteUser(User user)
+        {
+            Users.Remove(user);
+        }
+}
